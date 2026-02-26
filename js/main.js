6f2fb8bebee8206ed4823fc0931e1e6473eb4eb0
Hermann72 <rodriguezth@gmail.com>
2026-02-25 23:38:55 -0500

    Improve projects section UI

--- js/main.js
+++ js/main.js
@@ -3,3 +3,40 @@ function scrollToProjects() {
     behavior: "smooth",
   });
 }
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach((section) => {
+    const elementTop = section.getBoundingClientRect().top;
+    const visible = 120;
+
+    if (elementTop < windowHeight - visible) {
+      section.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
+
+/* Scroll correcto del navbar */
+
+document.querySelectorAll("nav a").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").replace("#", "");
+    const targetSection = document.getElementById(targetId);
+
+    const offset = 70; // altura del navbar
+    const position = targetSection.offsetTop - offset;
+
+    window.scrollTo({
+      top: position,
+      behavior: "smooth",
+    });
+  });
+});
